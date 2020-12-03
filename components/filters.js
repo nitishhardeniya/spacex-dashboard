c8d661904a7c2b0771ed2c4247250425723b9ed0
nitish <nitish.hardeniya@happay.in>
2020-12-03 20:29:33 +0530

    launch and land filter

--- components/filters.js
+++ components/filters.js
@@ -1,17 +1,33 @@
 import Grid from './Grid';
-const Filters = ({ filterByLaunch, filterByLanding }) => <Grid container>
-    
-    <Grid container>
-        <Grid item className="padding-10" >Successful Launch</Grid>
-        <Grid item xs={12}><div className="fl-buttons" onClick={()=>filterByLaunch(true)}>True</div></Grid>
-        <Grid item xs={12}><div className="fl-buttons" onClick={()=>filterByLaunch(false)}>False</div></Grid>
-    </Grid>
+import React, { useState } from 'react';
 
-    <Grid container>
-        <Grid item className="padding-10">Successful Landing</Grid>
-        <Grid item xs={12}><div className="fl-buttons" onClick={()=>filterByLanding(true)}>True</div></Grid>
-        <Grid item xs={12}><div className="fl-buttons" onClick={()=>filterByLanding(false)}>False</div></Grid>
-    </Grid>
-</Grid>;
+const Filters = ({ filterByLaunch, filterByLanding }) => {
+    const [ launchFilter, setLaunchFilter ] = useState(false);
+    const [ landingFilter, setLandingFilter ] = useState(false);
+
+    const _filterByLaunch = (status) => {
+        setLaunchFilter(status);
+        filterByLaunch(status);
+    }
+    
+    const _filterByLanding = (status) => {
+        setLandingFilter(status);
+        filterByLanding(status);
+    }
+
+    return <Grid container>
+        <Grid container>
+            <Grid item className="padding-10" >Successful Launch</Grid>
+            <Grid item xs={12}><div className={`fl-buttons ${launchFilter ? "active":""}`} onClick={()=>_filterByLaunch(true)}>True</div></Grid>
+            <Grid item xs={12}><div className={`fl-buttons ${!launchFilter ? "active":""}`} onClick={()=>_filterByLaunch(false)}>False</div></Grid>
+        </Grid>
+
+        <Grid container>
+            <Grid item className="padding-10">Successful Landing</Grid>
+            <Grid item xs={12}><div className={`fl-buttons ${landingFilter ? "active":""}`} onClick={()=>_filterByLanding(true)}>True</div></Grid>
+            <Grid item xs={12}><div className={`fl-buttons ${!landingFilter ? "active":""}`} onClick={()=>_filterByLanding(false)}>False</div></Grid>
+        </Grid>
+    </Grid>;
+};
 
 export default Filters;
